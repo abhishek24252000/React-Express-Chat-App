8c76c5e837a3f32258417cbd748f5a118e5fc3db
abhishek24252000 <aringaleabhishek@gmail.com>
2025-03-30 20:50:47 +0530

    implement user authentication with signup, login, and logout functionality; add profile update route and protect routes middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,14 @@
-import { User } from '../middleware/user.model.js';
+import { generateToken } from '../lib/utils.js';
+import { User } from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
 
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
     //check passwords length
     if (password.length < 6) {
       return res
@@ -28,16 +32,58 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-        //genearte jwt toke
-        
+      //genearte jwt toke
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       res.status(400).json({ message: 'Invalid user Data' });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log('error in signup contoller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
-export const login = (req, res) => {
-  res.send('login route');
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log('Error in login contoller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 export const logout = (req, res) => {
-  res.send('logout route');
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
+
+export const updateProfile = async (params) => {};
