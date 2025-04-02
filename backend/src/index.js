f5569fb66c0315e68633d6a77763c397b928787d
abhishek24252000 <aringaleabhishek@gmail.com>
2025-04-02 17:51:20 +0530

    add chat-app as a local dependency, update scripts for backend and frontend, and configure production static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,13 @@ import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import { app ,server} from './lib/socket.js';
 
+import path from "path";
+
 dotenv.config();
 // const app = express();
-
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve(); // Get the current directory name
 
 app.use(express.json({ limit: '5mb' })); //this allowes to extract json data out of request body in controller.ex:const {password,..}=req.body
 app.use(cookieParser());
@@ -18,6 +21,13 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 server.listen(PORT, () => {
   console.log('server runnig on 5001');
